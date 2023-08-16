2dcd32c9c99101f523ea6a91eeef4af40df04c23
Kingbragger <simple_shell.git>
2023-08-16 20:22:37 +0200

    0x04-python-more_data_structures

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def update_dictionary(a_dictionary, key, value):
+
+    """Changes or add key/value pairs in a dictionary."""
+
+    a_dictionary[key] = value
+
+    return (a_dictionary)
