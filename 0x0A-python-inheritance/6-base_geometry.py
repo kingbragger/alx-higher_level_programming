d3983bb155ea19dbf8f5eadb90664ce5f561901b
Kingbragger <ndlovuhenry73@gmail.com>
2023-09-11 12:18:08 +0200

    python_inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define a base geometry class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represents base geometry."""
+
+    def area(self):
+        """Not implemented."""
+        raise Exception("area() is not implemented")
